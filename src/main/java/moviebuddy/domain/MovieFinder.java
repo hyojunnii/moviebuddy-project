67286920f1b9ca691c4cd0f3d5463ba7e11b0c58
hyojunnii <96437859+hyojunnii@users.noreply.github.com>
2023-05-10 04:42:24 +0900

    #S7 - 빈 이름으로 자동 의존관계 주입하기
    같은 타입의 빈이 2개 이상 존재
    : 빈의 타입 외에 이름까지 활용해서 주입 (`@Qualifier()` 사용 또는 어노테이션에 이름속성부여)

--- src/main/java/moviebuddy/domain/MovieFinder.java
+++ src/main/java/moviebuddy/domain/MovieFinder.java
@@ -1,5 +1,6 @@
 package moviebuddy.domain;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,7 +13,7 @@ public class MovieFinder {
     private final MovieReader movieReader;
 
     //@Autowired // 생성자 하나면 생략 가능
-    public MovieFinder(MovieReader movieReader) {
+    public MovieFinder(@Qualifier("csvMovieReader") MovieReader movieReader) {
         this.movieReader = Objects.requireNonNull(movieReader);
     }
 
