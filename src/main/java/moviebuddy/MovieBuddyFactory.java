3f3bbdc9c8a8db7faf170b6b46909e0d15181a28
hyojunnii <96437859+hyojunnii@users.noreply.github.com>
2023-05-10 03:56:58 +0900

    #S6 - 자바 코드로 의존관계 주입하기
    자바 코드로 의존관계 주입
    : 메소드 파라미터 방식 이용

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,6 +2,7 @@ package moviebuddy;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +11,21 @@ import org.springframework.context.annotation.Configuration;
 public class MovieBuddyFactory {
 
     @Bean
-    public MovieFinder movieFinder() {
-        return new MovieFinder(new CsvMovieReader());
+    public MovieReader movieReader() {
+        return new CsvMovieReader();
+    }
+
+    /**
+     * 자바 코드로 의존관계 주입하는 방법
+     * 1. 메소드 콜 - movieReader() 직접 호출
+     * 2. 메소드 파라미터
+     * 3. 생성자
+     * 4. 애노테이션
+     */
+    @Bean
+    //@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) - 빈 스코프 미지정시 싱글톤
+    public MovieFinder movieFinder(MovieReader movieReader) {
+        return new MovieFinder(movieReader);
     }
 
 }
---
+++ src/test/java/moviebuddy/domain/BeanScopeTests.java
@@ -0,0 +1,21 @@
+package moviebuddy.domain;
+
+import moviebuddy.MovieBuddyFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanScopeTests {
+
+    @DisplayName("빈 스코프 테스트 - 싱글톤(단 하나의 객체) / 프로토타입(매번 객체 생성)")
+    @Test
+    void Equals_MovieFinderBean() {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
+        MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
+
+        Assertions.assertEquals(movieFinder, applicationContext.getBean(MovieFinder.class));
+    }
+
+}
