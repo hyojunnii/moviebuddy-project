49ec24130683d3458570984ef731725adbe6d81e
hyojunnii <96437859+hyojunnii@users.noreply.github.com>
2023-05-11 21:41:12 +0900

    #S13 - 환경추상화로 메타데이터 위치 설정하기
    VM options, environment 인터페이스 사용하여 메타데이터 위치 설정을 하도록 코드 변경
    .properties 학습 테스트 작성

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -3,17 +3,19 @@ package moviebuddy;
 import moviebuddy.data.CsvMovieReader;
 import moviebuddy.data.XmlMovieReader;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 // 빈 구성정보 (Configuration Metadata)
 @Configuration
+@PropertySource("/application.properties")
 @ComponentScan(basePackages = {"moviebuddy"}) // 자동 클래스 탐지 기법
 @Import({MovieBuddyFactory.DomainModuleConfig.class, MovieBuddyFactory.DataSourceModuleConfig.class})
 public class MovieBuddyFactory {
 
-   @Bean
-   public Jaxb2Marshaller jaxb2Marshaller() {
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         marshaller.setPackagesToScan("moviebuddy");
 
@@ -28,24 +30,31 @@ public class MovieBuddyFactory {
     @Configuration
     static class DataSourceModuleConfig {
 
-       @Profile(MovieBuddyProfile.CSV_MODE)
-       @Bean
-       public CsvMovieReader csvMovieReader() {
-           CsvMovieReader movieReader = new CsvMovieReader();
-           movieReader.setMetadata("movie_metadata.csv");
+        private final Environment environment;
 
-           return movieReader;
-       }
+        public DataSourceModuleConfig(Environment environment) {
+            this.environment = environment;
+        }
 
-        @Profile(MovieBuddyProfile.XML_MODE)
+        @Profile(MovieBuddyProfile.CSV_MODE)
         @Bean
-        public XmlMovieReader xmlMovieReader(Unmarshaller unmarshaller){
-            XmlMovieReader movieReader = new XmlMovieReader(unmarshaller);
-            movieReader.setMetadata("movie_metadata.xml");
+        public CsvMovieReader csvMovieReader() {
+            CsvMovieReader movieReader = new CsvMovieReader();
+
+            // 애플리케이션 외부에서 작성된 설정정보를 일겅, 메타데이터 위치 설정하기
+            movieReader.setMetadata(environment.getProperty("movie.metadata"));
 
             return movieReader;
         }
 
+        @Profile(MovieBuddyProfile.XML_MODE)
+        @Bean
+        public XmlMovieReader xmlMovieReader(Unmarshaller unmarshaller) {
+            XmlMovieReader movieReader = new XmlMovieReader(unmarshaller);
+            movieReader.setMetadata(environment.getProperty("movie.metadata"));
+
+            return movieReader;
+        }
     }
 
 }
